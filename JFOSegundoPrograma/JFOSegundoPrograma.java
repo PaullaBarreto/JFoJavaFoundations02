0b062e07125c428c73386da06e00e70fe77af33a
paullabarreto <anasilva.7259@aluno.saojudas.br>
2022-11-02 12:04:04 -0300

    JFO Segundo Programa

---
+++ JFOSegundoPrograma/JFOSegundoPrograma.java
@@ -0,0 +1,52 @@
+package JFOSegundoPrograma;
+
+public class JFOSegundoPrograma {
+
+    public static void main(String[] args) {
+    //Use 8 print statements to print a smiley face.
+    //Your art will rely on only a single character, besides space, such as X or #
+    System.out.println("  ####  ");
+    System.out.println(" #    # ");
+    System.out.println("# #  # #");
+    System.out.println("#      #");
+    System.out.println("# #  # #");
+    System.out.println("#  ##  #");
+    System.out.println(" #    # ");
+    System.out.println("  ####  ");
+
+    //Usar pelo menos oito instruções de impressão
+    //Ter pelo menos oito caracteres de largura
+    //Usar pelo menos 20 caracteres que não sejam espaço
+    System.out.println("     x       x    ");
+    System.out.println("    xx       xx    ");
+    System.out.println("   x  x_____x  x   ");
+    System.out.println("  x             x  ");
+    System.out.println(" x   xx     xx   x");
+    System.out.println("====     V     ====");
+    System.out.println("======(__I__)======");
+    System.out.println("   (            ) ");
+    System.out.println("    (__________)");
+
+    //Problema 3: a Snake Box Factory
+    
+    //Identifique três objetos neste cenário?
+    // 1 caixa de papelão, 2 processo de pedidos, 3 cobra transportadora são modelaveis
+    //Liste três propriedades e três comportamentos pertencentes a cada objeto
+    //Caixa de papelão: Celulose, pigmento e cola. 
+    //Comportamentos: quadrada, personalizada, qualidade
+    //Processo de pedidos: número do pedido, valor e produto. 
+    //Comportamentos: recebimento do pedido, confecção do pedido e envio do pedido
+    //Cobra transportadora: Especie, tonalidade, venenosa. 
+    //Comportamentos: pega a caixa vazia, leva para o preenchimento e entrega para o departamento de envio
+    //Solução:
+    //Confeccionar as caixas de papelão através de uma máquina de impressão 3D produto continuara a ser personalizado
+    // e mantendo o padrão de qualidade ja consolidado com os clientes.
+    //Extrair os pedidos de compras de forma impressa contendo número de pedido, dados do cliente, quantidade adquirida,
+    //endereço para entrega e valor da compra, usar essa impressão para preencher as caixas com os pedidos.
+    //Com uma esteira alocada entre os departamentos as caixa vazias vão passando e sendo preenchidas comforme o
+    //o pedido impresso devidamento anexado a caixa 
+    //para ser utilizado posteriormente como dados de envio para o cliente sendo apenas colado na caixa 
+    //após a finalização do pedido.
+
+    }
+}
